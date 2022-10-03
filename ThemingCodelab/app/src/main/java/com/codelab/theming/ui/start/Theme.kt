18cad4eb4524cd64d6ea1ff68cfefc09489a7e7e
fabspulkit <pulkitchausali.22@gmail.com>
2022-10-04 03:29:39 +0530

    Base Commit

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Theme.kt
@@ -0,0 +1,10 @@
+package com.codelab.theming.ui.start
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetNewsTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+
+}
